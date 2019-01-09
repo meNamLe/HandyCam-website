0708322bbf9bb2afdf9896e3489c9d79d5cd98e8
NamLe <vnamle1@gmail.com>
2019-01-09 01:46:45 -0500

    Updates

---
+++ precache-manifest.cde1e57abfb311d8cecfa13b584644e0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "afeae45da09e47d1c171c49d3455eff5",
+    "url": "/HandyCam-website/static/media/video.afeae45d.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/HandyCam-website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7ec2b5c953fe30b2636a3673d4fa8fdf",
+    "url": "/HandyCam-website/static/media/bless_up.7ec2b5c9.svg"
+  },
+  {
+    "revision": "f8312e2dcb815d8fd44097204779a628",
+    "url": "/HandyCam-website/static/media/HandyCam.f8312e2d.png"
+  },
+  {
+    "revision": "35bdbb307d25737584ef",
+    "url": "/HandyCam-website/static/js/runtime~main.35bdbb30.js"
+  },
+  {
+    "revision": "67740e02c582d1008535",
+    "url": "/HandyCam-website/static/js/main.67740e02.chunk.js"
+  },
+  {
+    "revision": "254fa0ed70ba87be19d7",
+    "url": "/HandyCam-website/static/js/1.254fa0ed.chunk.js"
+  },
+  {
+    "revision": "67740e02c582d1008535",
+    "url": "/HandyCam-website/static/css/main.5a9939f2.chunk.css"
+  },
+  {
+    "revision": "bd7b441f22816066ff2fab47e9fb9d6e",
+    "url": "/HandyCam-website/index.html"
+  }
+];
